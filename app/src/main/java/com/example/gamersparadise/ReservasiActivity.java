47d65bc4a506ac4d66cf87cdf350ebff02f726d5
grazpberry <hotmauligracia@gmail.com>
2024-04-04 05:01:22 +0700

    Reservasi 2 activity

--- app/src/main/java/com/example/gamersparadise/ReservasiActivity.java
+++ app/src/main/java/com/example/gamersparadise/ReservasiActivity.java
@@ -1,7 +1,12 @@
 package com.example.gamersparadise;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class ReservasiActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class ReservasiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservasi);
+
+        MaterialToolbar topAppBar = findViewById(R.id.top_app_bar);
+        setSupportActionBar(topAppBar);
+
+        getSupportActionBar().setTitle(null);
+
+        ImageView backButton = findViewById(R.id.toolbar_back_icon);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
