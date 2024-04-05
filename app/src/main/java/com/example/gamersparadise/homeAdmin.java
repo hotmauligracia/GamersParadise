9c9904bc69ebc13d153982cd08ca0941f6e0dd6d
Gracia Hotmauli <95269134+grazpberry@users.noreply.github.com>
2024-04-05 12:54:03 +0700

    pull (#40)
    * navbar and intent (#35)
    
    * solve error (#29)
    
    * Revert "Reservasi activity layout"
    
    * Update misc.xml
    
    * Revert "Revert "Reservasi activity layout"" (#28)
    
    * time_slot_item.xml
    
    * bottom_bar_reservasi.xml
    
    * bottom_bar_reservasi.xml
    
    * create package
    
    * reservasi2 activity done
    
    * navbar and intent
    
    * Balkis (#36)
    
    * home admin, promosi admin
    
    * home admin, promosi admin
    
    * home admin, promosi admin
    
    * home admin, promosi admin
    
    ---------
    
    Co-authored-by: Gracia Hotmauli <95269134+grazpberry@users.noreply.github.com>
    
    * update activities (#39)
    
    * activity promotion view dan top_bar.xml
    
    * profile_view, facility_view, progress history page
    
    * profile_view, facility_view, progress history page
    
    * Done history page, ubah button profile dan style secondary button
    
    * History selesai
    
    ---------
    
    Co-authored-by: Khaliza Fania <93924623+doncuwori@users.noreply.github.com>
    Co-authored-by: Khaliza Fania <khlzfan03@gmail.com>
    
    ---------
    
    Co-authored-by: amlbalqis <125745812+amlbalqis@users.noreply.github.com>
    Co-authored-by: Khaliza Fania <93924623+doncuwori@users.noreply.github.com>
    Co-authored-by: Khaliza Fania <khlzfan03@gmail.com>

---
+++ app/src/main/java/com/example/gamersparadise/homeAdmin.java
@@ -0,0 +1,14 @@
+package com.example.gamersparadise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin);
+    }
+}
