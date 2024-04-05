7a290b52f4fea0599293c949dd042c6eb51b3bd0
amaliabalqis <amaliabalqis26@gmail.com>
2024-04-05 08:08:00 +0700

    home admin, promosi admin

---
+++ app/src/main/java/com/example/gamersparadise/formResto_admin.java
@@ -0,0 +1,33 @@
+package com.example.gamersparadise;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class formResto_admin extends AppCompatActivity {
+    private Spinner spinnerTipeMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_form_resto_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        spinnerTipeMenu = findViewById(R.id.spinnerTipeMenu);
+        String[] tipe_menu = getResources().getStringArray(R.array.tipeMenu);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.spinner, tipe_menu);
+        adapter.setDropDownViewResource(R.layout.dropdown);
+        spinnerTipeMenu.setAdapter(adapter);
+    }
+}
