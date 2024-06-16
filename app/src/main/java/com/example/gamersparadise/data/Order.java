05bb3c27baf7f808ab16da3b669266b853d5734b
grazpberry <hotmauligracia@gmail.com>
2024-06-17 02:11:54 +0700

    file structure revised

---
+++ app/src/main/java/com/example/gamersparadise/data/Order.java
@@ -0,0 +1,89 @@
+package com.example.gamersparadise.data;
+
+import com.google.type.DateTime;
+
+import java.util.ArrayList;
+
+public class Order {
+    private String id;
+    private String userId;
+    private String reservationId;
+    private ArrayList<String> menuId;
+    private String promotionId;
+    private String totalPrice;
+    private String status;
+    private DateTime createdAt;
+    private DateTime updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(String reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public ArrayList<String> getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(ArrayList<String> menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getPromotionId() {
+        return promotionId;
+    }
+
+    public void setPromotionId(String promotionId) {
+        this.promotionId = promotionId;
+    }
+
+    public String getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(String totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public DateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(DateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ app/src/main/java/com/example/gamersparadise/data/Reservation.java
@@ -0,0 +1,114 @@
+package com.example.gamersparadise.data;
+
+import com.google.type.DateTime;
+
+public class Reservation {
+    private String id;
+    private String userId;
+    private String facilityId;
+    private String customerName;
+    private String customerPhone;
+    private DateTime reservationTime;
+    private String promotionId;
+    private String totalPrice;
+    private boolean isLate;
+    private String status;
+    private DateTime createdAt;
+    private DateTime updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFacilityId() {
+        return facilityId;
+    }
+
+    public void setFacilityId(String facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public DateTime getReservationTime() {
+        return reservationTime;
+    }
+
+    public void setReservationTime(DateTime reservationTime) {
+        this.reservationTime = reservationTime;
+    }
+
+    public String getPromotionId() {
+        return promotionId;
+    }
+
+    public void setPromotionId(String promotionId) {
+        this.promotionId = promotionId;
+    }
+
+    public String getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(String totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean isLate() {
+        return isLate;
+    }
+
+    public void setLate(boolean late) {
+        isLate = late;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public DateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(DateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
