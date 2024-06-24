e13e4afa6195e3cfb027dd0351bfa26fd7bf2376
hotmauligracia <hotmauligracia@gmail.com>
2024-06-24 07:24:36 +0700

    update

--- app/src/main/java/com/example/gamersparadise/customer/home/menu/ViewMenuActivity.java
+++ app/src/main/java/com/example/gamersparadise/customer/home/menu/ViewMenuActivity.java
@@ -1,12 +1,21 @@
 package com.example.gamersparadise.customer.home.menu;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gamersparadise.R;
+import com.example.gamersparadise.data.Menu;
 
 public class ViewMenuActivity extends AppCompatActivity {
+
+    private static final String TAG = "ViewMenuActivity";
+
+    private Menu menu;
+    private ImageView imgViewMenu;
+    private TextView tvViewFacilityName, tvViewFacilityCapacity, tvViewFacilityPrice, tvViewFacilityDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
