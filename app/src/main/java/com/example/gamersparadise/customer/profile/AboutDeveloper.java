471baef450ef0ef551a4b35b7206da4be444d96f
amlbalqis <amaliabalqis26@gmail.com>
2024-06-24 16:58:03 +0700

    update intent notifikasi, benerin layout order details di admin, benerin icon di admin

--- app/src/main/java/com/example/gamersparadise/customer/profile/AboutDeveloper.java
+++ app/src/main/java/com/example/gamersparadise/customer/profile/AboutDeveloper.java
@@ -1,6 +1,8 @@
 package com.example.gamersparadise.customer.profile;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gamersparadise.R;
@@ -10,5 +12,8 @@ public class AboutDeveloper extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_developer);
+
+        ImageView backButton = findViewById(R.id.toolbar_back_icon);
+        backButton.setOnClickListener(v -> onBackPressed());
     }
 }
